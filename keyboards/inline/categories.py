b9010a77107d611555b75e887c6cd070004767a4
Aliman09vv09 <104417109+Aliman09vv09@users.noreply.github.com>
2025-05-12 00:34:06 +0330

    Update categories.py

--- keyboards/inline/categories.py
+++ keyboards/inline/categories.py
@@ -4,13 +4,12 @@ from loader import db
 
 category_cb = CallbackData('category', 'id', 'action')
 
-
 def categories_markup():
-
     global category_cb
     
     markup = InlineKeyboardMarkup()
-    for idx, title in db.fetchall('SELECT * FROM categories'):
+    categories = db.fetchall('SELECT * FROM categories')  # Fetch categories
+    for idx, title in categories:
         markup.add(InlineKeyboardButton(title, callback_data=category_cb.new(id=idx, action='view')))
 
     return markup
